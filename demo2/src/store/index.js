0601852fa49a65a122a12a1ccbf6947a91e26bf6
全子修 <quanzixiu@qq.com>
2020-09-03 10:34:28 +0800

    初代版本

---
+++ demo2/src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    lists: [],
+    indexc:0,
+  },
+  mutations: {
+    addItem (statee, value){
+      statee.lists.push(value)
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
